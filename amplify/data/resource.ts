d64d23569dafe3bb51707ca2398d53678924323d
gnikhil98 <153263393+gnikhil98@users.noreply.github.com>
2024-07-01 17:19:47 +0530

    test

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -31,7 +31,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+
   },
 });
 
